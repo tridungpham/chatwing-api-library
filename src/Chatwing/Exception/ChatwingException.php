2865d4242fe2b5bd71711109f24e96db7826032d
Pham Tri Dung <dungpt@live.com>
2014-07-15 16:22:28 +0700

    Update ChatwingException class

--- src/Chatwing/Exception/ChatwingException.php
+++ src/Chatwing/Exception/ChatwingException.php
@@ -11,8 +11,15 @@ class ChatwingException extends \Exception
     protected $httpCode = 0;
     protected $params = array();
 
+    /**
+     * @param array   $errorData [description]
+     * @param integer $httpCode  [description]
+     */
     function __construct($errorData = array(), $httpCode = 0)
     {
+        if(is_string($errorData)) {
+            $errorData = array('message' => $errorData);
+        }
         $message = isset($errorData['message']) ? $errorData['message'] : '';
         $code = isset($errorData['code']) ? $errorData['code'] : 0;
         parent::__construct($message, $code, null);
